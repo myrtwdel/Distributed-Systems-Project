94e8fcfee806f33bf1fee56282686f05c160e7cd
myrtwdel <66162056+myrtwdel@users.noreply.github.com>
2024-02-24 17:03:33 +0200

    Delete test.py

--- 2.3/2.3.3/test.py
+++
@@ -1,9 +0,0 @@
-import random
-
-a = [0, 3, 6, 9]
-b = [round(random.uniform(0, 40), 1) for _ in range(10)]
-
-result = [b[i] for i in a]
-print(f" {b}")
-print("Τα στοιχεία του b στις θέσεις που δίνει το a:")
-print(result)
